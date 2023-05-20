f7b39d2c84680d4f6facf33a9d6d57925a199482
Tomás Catena <tomas.catena@protonmail.com>
2023-05-20 13:57:32 +0200

    fix script

--- bin/createNodeTsApp.js
+++ bin/createNodeTsApp.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const setup = require('./setup');
+const util = require('util');
+const { execSync } = require('child_process');
 
 // Validate arguments
 if (process.argv.length < 3) {
@@ -31,4 +33,79 @@ try {
   process.exit(1);
 }
 
-setup({ appPath, repo, folderName });
+const exec = util.promisify(require('child_process').exec);
+
+const runCmd = async (command) => {
+  try {
+    const { stdout, stderr } = await exec(command);
+
+    console.log(stdout);
+    console.log(stderr);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const hasYarn = () => {
+  try {
+    execSync('yarnpkg --version', { stdio: 'ignore' });
+
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const setup = async () => {
+  try {
+    // Clone repo
+    console.log(`Downloading files from repo ${repo}`);
+
+    await runCmd(`git clone --depth 1 ${repo} ${folderName}`);
+
+    console.log('Cloned successfully.');
+    console.log('');
+
+    // Change directory
+    process.chdir(appPath);
+
+    // Install dependencies
+    const useYarn = await hasYarn();
+    console.log('Installing dependencies...');
+
+    if (useYarn) {
+      await runCmd('yarn install');
+    } else {
+      await runCmd('npm install');
+    }
+
+    console.log('Dependencies installed successfully.');
+    console.log();
+
+    // Copy environment variables
+    fs.copyFileSync(path.join(appPath, '.env.example'), path.join(appPath, '.env'));
+    console.log('Environment files copied.');
+
+    // Delete .git folder
+    await runCmd('npx rimraf ./.git');
+
+    // Remove extra files
+    fs.unlinkSync(path.join(appPath, 'CHANGELOG.md'));
+    fs.unlinkSync(path.join(appPath, 'bin', 'createReactTsApp.js'));
+    fs.rmdirSync(path.join(appPath, 'bin'));
+
+    console.log('Installation is now complete!');
+    console.log();
+
+    console.log('We suggest that you start by typing:');
+    console.log(`    cd ${folderName}`);
+    console.log(useYarn ? '    yarn dev' : '    npm run dev');
+    console.log();
+    console.log('Enjoy your NodeJS Typescript boilerplate, which already supports a large number of ready-made features!');
+    console.log('Check README.md for more info.');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+setup();
